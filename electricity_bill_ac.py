dd45883df80681df59860f7f80ceba6cab294a84
sreyaharidas <151733059+sreyaharidas@users.noreply.github.com>
2026-09-18 10:47:19 +0530

    Add Fan_Units to new_data DataFrame

--- electricity_bill_ac.py
+++ electricity_bill_ac.py
@@ -28,7 +28,7 @@ elif Fan_Units<20 or Fan_Units>115:
 # Prediction button
 else:
     new_data = pd.DataFrame({
-        "AC_Units": [AC_Units]
+        "AC_Units": [AC_Units], "Fan_Units": [Fan_Units]
     })
 
     # Convert input to polynomial features

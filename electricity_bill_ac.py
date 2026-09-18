b22b44b6817b218d09a5bd866cb36e39ad7baf81
sreyaharidas <151733059+sreyaharidas@users.noreply.github.com>
2026-09-18 09:15:41 +0530

    Update electricity_bill_ac.py

--- electricity_bill_ac.py
+++ electricity_bill_ac.py
@@ -3,7 +3,7 @@ import joblib
 import pandas as pd
 
 # Load the saved Polynomial Regression model
-model = joblib.load("polynomial_regression_model(1).pkl")
+model = joblib.load("polynomial_regression_model.pkl")
 
 st.title("Electric Bill Prediction")
 st.write("Predict the electricity bill based on AC units.")
